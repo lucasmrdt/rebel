ec9994ad08d3a1b94d1a01a53ddd62d8b587af67
mmessaoudi <mmessaoudi@kaisertrot.nat.lamsade.dauphine.fr>
2023-04-27 22:51:25 +0200

    update train for LAMSADE server

--- src/train.py
+++ src/train.py
@@ -86,7 +86,7 @@ def train(conf: omegaconf.DictConfig) -> None:
     callbacks_store.append(LearningRateMonitor(logging_interval='step'))
     # trainer
     trainer = pl.Trainer(
-        # gpus=conf.gpus,
+        gpus=conf.gpus,
         accumulate_grad_batches=conf.gradient_acc_steps,
         gradient_clip_val=conf.gradient_clip_value,
         val_check_interval=conf.val_check_interval,
